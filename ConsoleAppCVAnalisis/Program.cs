6478d6fd38e663a3530d33bd26806d3ce4d20b1b
CesarSP99 <cesarandres00@gmail.com>
2021-05-25 12:34:12 -0400

    Claves de conexión agregadas

--- ConsoleAppCVAnalisis/Program.cs
+++ ConsoleAppCVAnalisis/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
 
 namespace ConsoleAppCVAnalisis
 {
     class Program
     {
+        static string subscriptionKey = "a28631f410ba4db0a5b90acaf35dc787";
+        static string endpoint = "https://cvcesar.cognitiveservices.azure.com/";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
