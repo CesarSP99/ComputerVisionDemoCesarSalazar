b8cb3b46a665064a76e949660f045ad6bfcb25a0
CesarSP99 <cesarandres00@gmail.com>
2021-05-25 12:24:54 -0400

    Add project files.

---
+++ ConsoleAppCVAnalisis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCVAnalisis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
